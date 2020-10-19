bace977f853e1bfbc004c044ba4f1a9da36894e1
jeanps7 <philip-j1@hotmail.com>
2020-10-19 12:03:21 -0300

    Atividade 7 e 8

---
+++ Atividade7/main.js
@@ -0,0 +1,9 @@
+var a = prompt("Digite o valor 1");
+var b = prompt("Digite o valor 2");
+var c = prompt("Digite o valor 3");
+
+function  Maior(a,b,c){
+    var Maior = Math.max(a,b,c);
+    return alert(Maior);
+}
+Maior(a,b,c);
---
+++ Atividade8/main.js
@@ -0,0 +1,18 @@
+var livro = {
+    Autor: "Stephen King",
+    Titulo: "Carrie",
+    Genero:"terror",
+}
+
+var objLivro = new Object();
+objLivro.Autor = "Stephen King";
+objLivro.Titulo = "Carrie";
+objLivro.Genero = "terror";
+
+var livro3 = {};
+var nome = "Autor";
+
+
+livro3[nome] = "Stephen King";
+
+
